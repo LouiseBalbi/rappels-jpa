0cfb23f5410ff75a8fd91d78a660345dc589fd61
LouiseBalbi <67003682+LouiseBalbi@users.noreply.github.com>
2020-12-16 13:25:12 +0100

    tp1 exo2

--- src/main/java/jpaAvecXml/TestJpa.java
+++ src/main/java/jpaAvecXml/TestJpa.java
@@ -4,13 +4,30 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
-public class TestJpa {
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.sun.tools.javac.Main;
+
+@SpringBootApplication
+public class TestJpa implements CommandLineRunner{
+
+	
 	public static void main(String[] args) {
 		
-		EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("pu_essai");
-		EntityManager em = entityManagerFactory.createEntityManager();
+		//exo 1
+//		EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("pu_essai");
+//		EntityManager em = entityManagerFactory.createEntityManager();
+		
+		SpringApplication.run(TestJpa.class, args);
 
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		
 	}
 
 }
